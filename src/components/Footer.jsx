9e1edffc36909dc77354d29de8730916e09f804c
blckvia <a.vyatkinn@icloud.com>
2022-11-16 18:40:48 +0500

    begining

---
+++ src/components/Footer.jsx
@@ -0,0 +1,21 @@
+function Footer() {
+    return (
+        <footer className='page-footer cyan lighten-1'>
+            <div className='footer-copyright'>
+                <div className='container'>
+                    © {new Date().getFullYear()} Copyright Text
+                    <a
+                        className='github grey-text text-lighten-4 right'
+                        href='!#'
+                        target='_blank'
+                        rel='noreferrer'
+                    >
+                        GitHub
+                    </a>
+                </div>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,28 @@
+function Header() {
+    return (
+        <nav className='cyan darken-2'>
+            <div className='nav-wrapper'>
+                <a
+                    href='!#'
+                    className='brand-logo'
+                >
+                    Fortnite Shop
+                </a>
+                <ul
+                    id='nav-mobile'
+                    className='right hide-on-med-and-down'
+                ></ul>
+                <a
+                    className='github right'
+                    href='https://github.com/Blckvia/fortnite-shop'
+                    target='_blank'
+                    rel='noreferrer'
+                >
+                    GitHub
+                </a>
+            </div>
+        </nav>
+    );
+}
+
+export default Header;
